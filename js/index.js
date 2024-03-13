53fdedf75dcea326c311baacab1f1842263c6320
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-03-13 14:40:33 -0300

    principio del toogler para cambiar de tema-color

--- js/index.js
+++ js/index.js
@@ -3,6 +3,36 @@ document.addEventListener("touchmove",()=>{
     const navBar = document.querySelector("#navBar")
 
     if (navBar && navBar.contains(event.target)) {
-        event.preventDefault(); // Prevenir el desplazamiento táctil dentro del menú desplegable
+        event.preventDefault(); 
     }
 }, { passive: false }); 
+
+
+
+const toogler = document.querySelector("#toogler")
+
+toogler.addEventListener("click",()=>{
+
+    const tema = document.querySelector(".dark")
+
+    if(tema){
+
+        
+        document.body.classList.replace("dark","light")
+        document.body.style.background="black"
+        const active = document.querySelector(".tema-click")
+        active.classList.add("tema-active")
+
+    }
+
+
+    else {
+
+        document.body.classList.replace("light","dark")
+        document.body.style.background="#ddd"
+        const active = document.querySelector(".tema-click")
+        active.classList.remove("tema-active")
+
+    }
+
+})
