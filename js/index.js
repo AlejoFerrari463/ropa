439518216ead4db0c9d741291f3e102c762a8b27
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-03-14 14:02:35 -0300

    avanzando con el toogler de cambiar tema de la pagina. index casi terminado

--- js/index.js
+++ js/index.js
@@ -20,8 +20,21 @@ toogler.addEventListener("click",()=>{
         
         document.body.classList.replace("dark","light")
         document.body.style.background="black"
+
+        const header = document.querySelector(".navbar")
+        header.style.background="black"
+        header.style.boxShadow="0px 0px 15px white"
+
         const active = document.querySelector(".tema-click")
         active.classList.add("tema-active")
+
+        const carousel = document.querySelector(".carousel")
+        carousel.style.background = "black"
+        carousel.style.boxShadow = "0px 0px 20px white"
+        const carouselTam = carousel.querySelectorAll(".carrouselTam")
+        carouselTam.forEach((element)=>{
+            element.style.background="black"
+        })
 
     }
 
@@ -29,9 +42,24 @@ toogler.addEventListener("click",()=>{
     else {
 
         document.body.classList.replace("light","dark")
-        document.body.style.background="#ddd"
+        document.body.style.background="white"
+
+        const header = document.querySelector(".navbar")
+        header.style.background="white"
+        header.style.boxShadow="0px 0px 15px black"
+
+
         const active = document.querySelector(".tema-click")
         active.classList.remove("tema-active")
+
+        const carousel = document.querySelector(".carousel")
+        carousel.style.background = "white"
+        carousel.style.boxShadow = "0px 0px 20px black"
+        const carouselTam = carousel.querySelectorAll(".carrouselTam")
+        carouselTam.forEach((element)=>{
+            element.style.background="white"
+        })
+        
 
     }
 
