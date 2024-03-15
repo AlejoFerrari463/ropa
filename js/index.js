52c68fd9ba4998236f776a50641cd62b496e28b9
AlejoFerrari463 <alejoferrari463@gmail.com>
2024-03-15 09:45:39 -0300

    agregado de temas en las cards, falta sacarle fondo a las imagenes. falta tema en seccion contacto

--- js/index.js
+++ js/index.js
@@ -13,27 +13,35 @@ const toogler = document.querySelector("#toogler")
 
 toogler.addEventListener("click",()=>{
 
-    const tema = document.querySelector(".dark")
+    const tema = document.querySelector(".light")
+
+    const currentPath = window.location.pathname;
 
     if(tema){
 
-        
-        document.body.classList.replace("dark","light")
-        document.body.style.background="black"
+ 
+
+
+        document.body.classList.replace("light","dark")
+        document.body.style.background="#1C1C1C"
 
         const header = document.querySelector(".navbar")
-        header.style.background="black"
+        header.style.background="#1C1C1C"
         header.style.boxShadow="0px 0px 15px white"
 
         const active = document.querySelector(".tema-click")
         active.classList.add("tema-active")
 
-        const carousel = document.querySelector(".carousel")
-        carousel.style.background = "black"
+        // INDEX
+        
+        if (currentPath == "/index.html"){
+
+            const carousel = document.querySelector(".carousel")
+        carousel.style.background = "#1C1C1C"
         carousel.style.boxShadow = "0px 0px 20px white"
         const carouselTam = carousel.querySelectorAll(".carrouselTam")
         carouselTam.forEach((element)=>{
-            element.style.background="black"
+            element.style.background="#1C1C1C"
         })
 
         const footer = document.querySelector("footer")
@@ -43,12 +51,30 @@ toogler.addEventListener("click",()=>{
         const p = footer.querySelector("p")
         p.style.color = "white"
 
+        }
+
+        // CARDS
+        if (currentPath == "/html/ofertas.html" || currentPath == "/html/hombres.html" || currentPath == "/html/mujeres.html"){
+
+            const cards = document.querySelectorAll(".card")
+            cards.forEach((element)=>{
+                element.style.background="#1C1C1C"
+                element.style.boxShadow="0px 0px 10px white"
+                element.style.color = "white"
+            })
+
+
+        }
+        
+
+    
+
     }
 
 
     else {
 
-        document.body.classList.replace("light","dark")
+        document.body.classList.replace("dark","light")
         document.body.style.background="white"
 
         const header = document.querySelector(".navbar")
@@ -59,20 +85,39 @@ toogler.addEventListener("click",()=>{
         const active = document.querySelector(".tema-click")
         active.classList.remove("tema-active")
 
-        const carousel = document.querySelector(".carousel")
-        carousel.style.background = "white"
-        carousel.style.boxShadow = "0px 0px 20px black"
-        const carouselTam = carousel.querySelectorAll(".carrouselTam")
-        carouselTam.forEach((element)=>{
-            element.style.background="white"
-        })
+        if (currentPath == "/index.html"){
 
-        const footer = document.querySelector("footer")
-        const span = footer.querySelector("span")
-        span.classList.replace("text-light","text-dark")
+            const carousel = document.querySelector(".carousel")
+            carousel.style.background = "white"
+            carousel.style.boxShadow = "0px 0px 20px black"
+            const carouselTam = carousel.querySelectorAll(".carrouselTam")
+            carouselTam.forEach((element)=>{
+                element.style.background="white"
+            })
+    
+            const footer = document.querySelector("footer")
+            const span = footer.querySelector("span")
+            span.classList.replace("text-light","text-dark")
+    
+            const p = footer.querySelector("p")
+            p.style.color = "black"
 
-        const p = footer.querySelector("p")
-        p.style.color = "black"
+
+        }
+
+         // CARDS
+         if (currentPath == "/html/ofertas.html" || currentPath == "/html/hombres.html" || currentPath == "/html/mujeres.html"){
+
+            const cards = document.querySelectorAll(".card")
+            cards.forEach((element)=>{
+                element.style.background="white"
+                element.style.boxShadow="0px 0px 10px black"
+                element.style.color = "black"
+            })
+
+
+        }
+      
         
 
     }
